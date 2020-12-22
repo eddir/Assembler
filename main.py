6baf6d89c0a071838a1e6175c0d0139212ac1940
Eduard <mail@rostkov.pro>
2020-12-22 20:34:10 +0400

    Ввод файла asm в качестве аргумента командной строки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 
 import Code
@@ -14,8 +15,14 @@ class Assembler:
     def __init__(self):
         """Открытие выходного потока и задание параметров
         """
-        self.ASM_FILE = os.path.abspath("./Prog.asm")  # Входной поток
-        self.HACK_FILE = os.path.abspath("./Prog.hack")  # Выходной поток
+
+        self.input_file = "Prog.asm"
+
+        if len(sys.argv) > 1:
+            self.input_file = sys.argv[1]
+
+        self.ASM_FILE = os.path.abspath(self.input_file)  # Входной поток
+        self.HACK_FILE = os.path.abspath("Prog.hack")  # Выходной поток
 
         # Открываем файл для записи результирующего бинарного файла
         self.output = open(self.HACK_FILE, 'w')
@@ -72,15 +79,16 @@ class Assembler:
                     raise ValueError("Invalid command %s" % parser.current_command)
 
         except IOError:
-            print("Файл Prog.asm не удалось открыть. Мы ожидаем, что он лежит по пути: {0} . Пожалуйста, "
+            print("Файл {0} не удалось открыть. Мы ожидаем, что он лежит по пути: {1} . Пожалуйста, "
                   "удостоверьтесь, что по указанному пути существует этот файл и на него прописаны права для чтения."
-                  .format(self.ASM_FILE))
+                  .format(self.input_file, self.ASM_FILE))
         finally:
             # Закрываем файл, чтобы сохранить изменения
             self.output.close()
 
 
 if __name__ == '__main__':
+    print("***\nПрограмма ассемблер, выполненная по инструкций от nand2tetris, студента группы бПИНЖ21 Росткова Э.А.\n***\n")
     print('Запуск ассемблера...')
 
     time.sleep(0.5)
